a5612686a24eb21555fbd6b1d025281497c01771
dv-jamie <sjm2506@gmail.com>
2023-01-31 00:35:55 +0900

    joinArea 작업 완료

---
+++ src/components/JoinArea.js
@@ -0,0 +1,21 @@
+import { Group130 } from '../assets/illust';
+import styles from "./JoinArea.module.css";
+
+function JoinArea() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.content_wrap}>
+                <div className={styles.text_wrap}>
+                    <div className={styles.title}>신학기 전에</div>
+                    <div className={styles.title}>미리 만나요!</div>
+                    <div className={styles.date}>2023.02.16</div>
+                    <div className={styles.coming}>COMING SOON</div>
+                </div>
+                <Group130 />
+            </div>
+                <button className={styles.join_button}>회원 가입하기</button>
+        </div>
+    );
+}
+
+export default JoinArea;
